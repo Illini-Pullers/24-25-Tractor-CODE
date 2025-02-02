5ec4faaa8cbbec86db1dd90d4653bfccbc5bfc65
AbhirajBhashkar <132953698+AbhirajBhashkar@users.noreply.github.com>
2025-02-01 18:47:36 -0600

    2

--- Controller/KILLSWITCH.h
+++
@@ -1,4 +0,0 @@
-#ifndef _795E3DC8_8E01_4FF0_A637_A226487E75EF__H_
-#define _795E3DC8_8E01_4FF0_A637_A226487E75EF__H_
-extern void CopyValidUtf8CodeUnits(U8 * DestUtf8Buffer, const U8 * SourceUtf8Buffer, U16 * Count, BOOL ZeroTerm);
-#endif
--- Controller/_NVAllocation.h
+++
@@ -1,1 +0,0 @@
-#define _NVSize 1
